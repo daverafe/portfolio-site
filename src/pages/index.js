a9554a64337f8f07ab1e69568c3931a68436588e
David Rafe <david.rafe@gmail.com>
2021-10-14 12:18:14 -0400

    successfully added link to art market to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import {Link} from 'gatsby' 
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making a gatsby site</p>
+      <p>Here's a link to a site I built</p>
+      <Link to="https://epic-allen-47afe0.netlify.app/">Art Market</Link>
     </Layout>
   )
 }
