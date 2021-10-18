ea6bd63049d8e28627da3e57de9d219565c334c9
David Rafe <david.rafe@gmail.com>
2021-10-18 12:04:01 -0400

    added content to about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,21 @@ import ProjectContainer from '../components/projectContainer'
 
 const IndexPage = () => {
 
-  const artMarket = {
+ const artMarket = {
     title: "Art Market",
     url: "https://epic-allen-47afe0.netlify.app/",
     description: "Marketplace / E-commerce application where users can browse and search through various local art pieces for purchase, and set up an account to upload and sell their own pieces of art."
-  }
-  const queue = {
+}
+const queue = {
     title: "Queue",
     url: "https://stormy-reaches-82175.herokuapp.com/",
     description: "A TV/Movie/Media recommendation app where users can create an account, and recommend content to other users that will populate on their profiles. "
-  }
-  const lifestyleRoster = {
+}
+const lifestyleRoster =  {
     title: "Lifetsyle Roster",
     url: "https://vigorous-hoover-20c685.netlify.app/",
     description: "Pinterest / Registry style app that allows you to set up categorized rosters of all your favorite items from across the web, creating gift or lifestyle inspiration. "
-  }
+}
 
   return (
     <Layout pageTitle="Home Page">
